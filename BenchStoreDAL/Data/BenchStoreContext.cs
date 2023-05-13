0baa3ea81111189faff425ea7cd34f40b0bf78cd
Alex Herčko <xhercko@fi.muni.cz>
2023-05-13 11:37:58 +0200

    add dbcontext

---
+++ BenchStoreDAL/Data/BenchStoreContext.cs
@@ -0,0 +1,17 @@
+﻿using BenchStoreDAL.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchStoreDAL.Data
+{
+    public class BenchStoreContext : DbContext
+    {
+        public BenchStoreContext(DbContextOptions<BenchStoreContext> options)
+            : base(options) { }
+
+        public DbSet<ResultEntry> ResultEntry { get; set; }
+        public DbSet<Result> Result { get; set; }
+        public DbSet<Label> Label { get; set; }
+    }
+}
+
