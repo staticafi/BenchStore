2508711aead79f9417d4c48a706fa49c217f303e
Alex Herčko <xhercko@fi.muni.cz>
2023-05-13 12:30:53 +0200

    add bl service registration

---
+++ BenchStoreBL/ServiceRegistration.cs
@@ -0,0 +1,43 @@
+﻿using BenchStoreBL.Options;
+using BenchStoreBL.Services.Labels;
+using BenchStoreBL.Services.ResultEntries;
+using BenchStoreBL.Services.ResultParsing;
+using BenchStoreBL.Services.Results;
+using BenchStoreBL.Services.ResultStoring;
+using BenchStoreBL.Services.ScriptExecution;
+using BenchStoreBL.Services.XMLElementParsing;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BenchStoreBL
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection RegisterBLConfig(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.Configure<StorageOptions>(
+                configuration.GetSection(StorageOptions.Storage));
+
+            serviceCollection.Configure<TableGeneratorOptions>(
+                configuration.GetSection(TableGeneratorOptions.TableGenerator));
+
+            return serviceCollection;
+        }
+
+        public static IServiceCollection RegisterBLServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IResultEntriesService, ResultEntriesService>();
+            serviceCollection.AddScoped<IResultsService, ResultService>();
+            serviceCollection.AddScoped<ILabelsService, LabelsService>();
+            serviceCollection.AddScoped<IXMLElementParser, XMLElementParser>();
+            serviceCollection.AddScoped<IResultParser, XMLResultParser>();
+            serviceCollection.AddScoped<IFileStoring, XMLFileStoring>();
+            serviceCollection.AddScoped<IScriptExecutor, PythonExecutor>();
+            serviceCollection.AddScoped<ITableGeneratorExecutor, TableGeneratorExecutor>();
+
+            return serviceCollection;
+        }
+    }
+}
+
