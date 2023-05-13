591c0e7e860b61aa792de998f2f19d5108879f6d
Alex Herčko <xhercko@fi.muni.cz>
2023-05-13 12:08:51 +0200

    add configuration options for bl

---
+++ BenchStoreBL/Options/StorageOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace BenchStoreBL.Options
+{
+    public class StorageOptions
+    {
+        public const string Storage = "Storage";
+
+        public string ResultStoragePath { get; set; } = string.Empty;
+    }
+}
+
---
+++ BenchStoreBL/Options/TableGeneratorOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace BenchStoreBL.Options
+{
+    public class TableGeneratorOptions
+    {
+        public const string TableGenerator = "TableGenerator";
+
+        public string PythonPath { get; set; } = string.Empty;
+        public string TableGeneratorPath { get; set; } = string.Empty;
+    }
+}
+
