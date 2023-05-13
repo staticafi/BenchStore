174842a1e81f651534c7ad8bd0aaad3dd635fc85
Alex Herčko <xhercko@fi.muni.cz>
2023-05-13 12:35:19 +0200

    add browsing of stored result files

--- BenchStoreMVC/Program.cs
+++ BenchStoreMVC/Program.cs
@@ -1,7 +1,12 @@
 using BenchStoreBL;
+using BenchStoreBL.Options;
 
 using BenchStoreDAL;
 using BenchStoreDAL.Data;
+
+using Microsoft.Extensions.FileProviders;
+
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,6 +44,32 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+string resultStorage;
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    IOptions<StorageOptions> options = services.GetRequiredService<IOptions<StorageOptions>>();
+    resultStorage = options.Value.ResultStoragePath;
+}
+
+PhysicalFileProvider fileProvider = new PhysicalFileProvider(resultStorage);
+string requestPath = "/Results";
+
+// Enable displaying browser links.
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = fileProvider,
+    RequestPath = requestPath
+});
+
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
+{
+    FileProvider = fileProvider,
+    RequestPath = requestPath
+});
+
 
 app.UseAuthorization();
 
