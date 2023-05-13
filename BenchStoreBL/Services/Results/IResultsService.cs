1cf5c6bd77184af6f33c6d5b21ac383ab2407bfb
Alex Herčko <xhercko@fi.muni.cz>
2023-05-13 12:21:33 +0200

    add results service

---
+++ BenchStoreBL/Services/Results/IResultsService.cs
@@ -0,0 +1,11 @@
+﻿using BenchStoreBL.Models;
+
+namespace BenchStoreBL.Services.Results
+{
+    public interface IResultsService
+    {
+        Task<Result?> GetResultByID(int id);
+        Task<Result?> GetResultByResultEntryID(int resultEntryID);
+    }
+}
+
---
+++ BenchStoreBL/Services/Results/ResultService.cs
@@ -0,0 +1,49 @@
+﻿using BenchStoreBL.Exceptions;
+using BenchStoreBL.Models;
+using BenchStoreBL.Models.Mappers;
+
+using BenchStoreDAL.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+using Entities = BenchStoreDAL.Entities;
+
+namespace BenchStoreBL.Services.Results
+{
+    internal class ResultService : IResultsService
+    {
+        private readonly BenchStoreContext _context;
+
+        public ResultService(BenchStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result?> GetResultByID(int id)
+        {
+            if (_context.Result == null)
+            {
+                throw ExceptionThrowerHelper.ThrowWhenDbSetIsNull<Entities.Result>();
+            }
+
+            Entities.Result? resultEntity = await _context.Result.FindAsync(id);
+            return resultEntity?.MapToModel(true);
+        }
+
+        public async Task<Result?> GetResultByResultEntryID(int resultEntryID)
+        {
+            if (_context.ResultEntry == null)
+            {
+                throw ExceptionThrowerHelper.ThrowWhenDbSetIsNull<Entities.Result>();
+            }
+
+            Entities.ResultEntry? resultEntryEntity = await _context
+                .ResultEntry
+                .Include(re => re.Result)
+                .FirstOrDefaultAsync(re => re.ID == resultEntryID);
+
+            return resultEntryEntity?.Result.MapToModel(true);
+        }
+    }
+}
+
