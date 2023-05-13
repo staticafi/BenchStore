63ac0586e5cf888423fd9025530c4fe5eb220c52
Alex Herčko <xhercko@fi.muni.cz>
2023-05-13 12:09:43 +0200

    add xml element serializing

---
+++ BenchStoreBL/Services/XMLElementParsing/IXMLElementParser.cs
@@ -0,0 +1,11 @@
+﻿using BenchStoreBL.XMLData;
+
+namespace BenchStoreBL.Services.XMLElementParsing
+{
+    public interface IXMLElementParser
+    {
+        TElement ParseXMLElement<TElement>(Stream stream)
+            where TElement : IXMLElement;
+    }
+}
+
---
+++ BenchStoreBL/Services/XMLElementParsing/XMLElementParser.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+using BenchStoreBL.XMLData;
+
+namespace BenchStoreBL.Services.XMLElementParsing
+{
+    internal class XMLElementParser : IXMLElementParser
+    {
+        public TElement ParseXMLElement<TElement>(Stream stream)
+            where TElement : IXMLElement
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            //settings.Async = true;
+            settings.ValidationType = ValidationType.DTD;
+            settings.ValidationFlags = System.Xml.Schema.XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.DtdProcessing = DtdProcessing.Parse;
+            settings.XmlResolver = new XmlUrlResolver();
+            settings.ValidationEventHandler += (sender, args) =>
+            {
+                Console.WriteLine($"XML Parser: {args.Message}");
+            };
+
+            using XmlReader reader = XmlReader.Create(stream, settings);
+            var xmlSerializer = new XmlSerializer(typeof(TElement));
+            TElement? xmlElement = (TElement?)xmlSerializer.Deserialize(reader);
+
+            if (xmlElement == null)
+            {
+                throw new ArgumentException("Failed to serialize result from XML!");
+            }
+
+            return xmlElement;
+        }
+    }
+}
+
